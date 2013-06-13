864ec2a3e64221293bbde02dcce1233b94eea098
Xavier Dutoit <xavier@tttp.eu>
2013-06-13 22:56:21 +0200

    original 4.3 version

---
+++ README.md
@@ -0,0 +1,9 @@
+Improves the event screens of civicrm. 
+
+The list of participants is optimised to increase entropy and remove redondant information. 
+
+For instance, it when you list the participants of an event, it removes the event name column, as it obviously always contains the name of the event. 
+
+As a rule, if a column always contain the same values, it's removed (eg an event without fee won't have money related columns)
+
+
--- eventisim.php
+++ eventisim.php
@@ -2,6 +2,71 @@
 
 require_once 'eventisim.civix.php';
 
+function eventisim_civicrm_buildForm ( $formName, &$form ){
+  if ("CRM_Event_Form_Search" != $formName) 
+    return;
+  $skip_columns = array();
+  $values=$form->GetVar("_values");
+  if (is_array($values) && array_key_exists("event_id",$values)) {
+      $skip_columns[]="event_id";
+  }
+  $cids;
+  $rows=$form->getTemplate()->get_template_vars("rows");
+  if (empty($rows))
+    return;
+  foreach ($rows as $row) {
+    $cids[]= (int) $row["contact_id"];
+  }
+  $cids = implode (",",$cids);
+  $query  = "SELECT id, organization_name as employer FROM civicrm_contact WHERE id in ($cids)";
+  $dao = CRM_Core_DAO::executeQuery($query, $params);
+  $orgs = array();
+  while ($dao->fetch()) {
+    $orgs[$dao->id] = $dao->employer;
+  }
+  foreach ($rows as &$row) {
+    if (array_key_exists($row["contact_id"],$orgs))
+      $row["employer"] = $orgs[$row["contact_id"]];
+    else
+      $row["employer"] = null; 
+  }
+ 
+  $ref=$rows[0];
+  foreach ($ref as $k => $field) {
+    $skip=true;
+    foreach($rows as $row) {
+      if ($row[$k] != $field) {
+        $skip=false;
+        break;
+      }
+    }
+    if ($skip) {
+      $skip_columns[]=$k;
+    }
+  }
+  $fieldalias=array ("fee_level"=>"participant_fee_level","fee_amount"=>"participant_fee_amount");
+  $columnHeaders=$form->getTemplate()->get_template_vars("columnHeaders");
+  array_splice( $columnHeaders, 2, 0, array(0=>array("field"=>"employer","name"=>"Organisation")) );
+  foreach ($columnHeaders as $k=>&$header) {
+    if (array_key_exists("sort",$header)) {
+      if (array_key_exists($header["sort"],$fieldalias)) {
+        $header["field"] = $fieldalias[$header["sort"]];
+      } else {
+        $header["field"] = $header["sort"];
+      }
+    } else {
+        $header["field"] = null;
+    }
+    if (in_array($header["field"],$skip_columns)) {
+      unset($columnHeaders[$k]);
+    }
+  }
+
+  $form->assign("rows",$rows);
+  $form->assign("columnHeaders",$columnHeaders);
+  $form->assign("skip_columns",$skip_columns);
+}
+
 /**
  * Implementation of hook_civicrm_config
  */
